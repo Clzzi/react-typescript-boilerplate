8b7abee955ffa0bb5647cdbe42f3ae8b880cded0
Clzzi <clzzi1109@naver.com>
2021-10-19 22:09:06 +0900

    Add: webpack config dev/comomn

---
+++ config/webpack.config.common.js
@@ -0,0 +1,43 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.tsx",
+  output: {
+    path: path.join(__dirname, "../dist"),
+    filename: "bundle.js",
+    publicPath: "/",
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"],
+    modules: ["node_modules"],
+    alias: {
+      "@": path.resolve(__dirname, "../src/"),
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        loader: "ts-loader",
+        exclude: /node_modules/,
+        options: {
+          transpileOnly: true,
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin({
+      cleanStaleWebpackAssets: false,
+    }),
+    new HtmlWebpackPlugin({
+      template: "../src/index.html",
+    }),
+  ],
+};
---
+++ config/webpack.config.dev.js
@@ -0,0 +1,16 @@
+const { merge } = require("webpack-merge");
+const common = require("./webpack.config.common.js");
+
+module.exports = merge(common, {
+  mode: "development",
+  devtool: "inline-source-map",
+  devServer: {
+    static: {
+      directory: "../dist",
+      host: "localhost",
+      port: 8080,
+      open: true,
+      historyApiFallback: true,
+    },
+  },
+});
